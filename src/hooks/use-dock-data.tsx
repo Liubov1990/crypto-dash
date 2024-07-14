d5182d9058919957fe2ffdc0773737842330bbad
Liuba <1990yavorska@gmail.com>
2024-07-14 12:05:26 +0200

    feat: add initial resizable layout for home page

---
+++ src/hooks/use-dock-data.tsx
@@ -0,0 +1,112 @@
+import { useCallback, useMemo, useState } from "react";
+import { LayoutData, TabBase } from "rc-dock";
+
+const tabCommonProps = {
+  minWidth: 240,
+  minHeight: 180,
+  group: "shared",
+};
+
+const tabsContents = {
+  news: {
+    ...tabCommonProps,
+    id: "news",
+    title: "News",
+    content: <div>News</div>,
+  },
+  comparance: {
+    ...tabCommonProps,
+    id: "comparance",
+    title: "Comparance graph",
+    content: <div>Comparance graph content</div>,
+  },
+  cryptocurrencies: {
+    ...tabCommonProps,
+    id: "cryptocurrencies",
+    title: "Cryptocurrencies",
+    content: <div>Cryptocurrencies</div>,
+  },
+  overview: {
+    ...tabCommonProps,
+    id: "overview",
+    title: "Market overview",
+    content: <div>Market overview</div>,
+  },
+  trends: {
+    ...tabCommonProps,
+    id: "trends",
+    title: "Trends",
+    content: <div>Trends</div>,
+  },
+};
+
+const initialLayout = {
+  dockbox: {
+    mode: "vertical",
+    children: [
+      {
+        mode: "horizontal",
+        size: 260,
+        children: [
+          {
+            tabs: [tabsContents.news],
+          },
+          {
+            tabs: [tabsContents.comparance],
+          },
+        ],
+      },
+      {
+        size: 180,
+        tabs: [tabsContents.cryptocurrencies],
+      },
+      {
+        size: 260,
+        mode: "horizontal",
+        children: [
+          {
+            tabs: [tabsContents.overview],
+          },
+          {
+            mode: "horizontal",
+            tabs: [tabsContents.trends],
+          },
+        ],
+      },
+    ],
+  },
+};
+
+function useDockData() {
+  const [layout, setLayout] = useState<LayoutData>(initialLayout as LayoutData);
+  const [isActive, setIsActive] = useState(true);
+
+  const groups = useMemo(
+    () => ({
+      shared: {
+        floatable: false,
+        maximizable: isActive,
+      },
+    }),
+    [isActive]
+  );
+
+  const loadTab = useCallback(
+    ({ id }: TabBase) => tabsContents[id as keyof typeof tabsContents],
+    [tabsContents]
+  );
+
+  const onLayoutChange = useCallback((newLayout: LayoutData) => {
+    setLayout(newLayout);
+  }, []);
+
+  return {
+    layout,
+    groups,
+    isActive,
+    loadTab,
+    onLayoutChange,
+  };
+}
+
+export default useDockData;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,32 @@
+import "./index.css";
+
+import { CSSProperties } from "react";
+import cn from "classnames";
+import DockLayout from "rc-dock";
+import useDockData from "../../hooks/use-dock-data";
+
+const ROOT_STYLES: CSSProperties = {
+  position: "absolute",
+  left: 30,
+  top: 30,
+  right: 30,
+  bottom: 30,
+};
+
 function Home(): React.ReactNode {
-  return <></>;
+  const { layout, groups, isActive, loadTab, onLayoutChange } = useDockData();
+
+  return (
+    <div className={cn("home", { "home--static": !isActive })}>
+      <DockLayout
+        layout={layout}
+        loadTab={loadTab}
+        onLayoutChange={onLayoutChange}
+        groups={groups}
+        style={ROOT_STYLES}
+      />
+    </div>
+  );
 }
 
 export default Home;
